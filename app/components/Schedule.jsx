20d9c5b8215b0dafdf1d9c7b5c1e6fb67c04fb46
Isham Faizal <i@adimaja.com>
2016-10-01 16:03:09 +0700

    Refactored Schedule

--- app/components/Schedule.jsx
+++ app/components/Schedule.jsx
@@ -7,6 +7,47 @@ import ScheduleItem from './ScheduleItem'
 import ScheduleDropTarget from './ScheduleDropTarget'
 import Course from './Course'
 
+class BaseSchedule extends React.Component {
+  render () {
+    const {schedule} = this.props
+
+    return (
+      <div className='schedule' style={gridScheduleStyle}>
+        {scheduleDropTargets()}
+        {schedule.valueSeq().map((s) => (
+          <ScheduleItem
+            day={s.getIn(['time', 'day'])}
+            slot={s.getIn(['time', 'slot'])}
+            duration={s.get('duration')}
+            key={s.get('name')}
+          >
+            <Course name={s.get('name')} room={s.get('room')} />
+          </ScheduleItem>
+        )).toJS()}
+      </div>
+    )
+  }
+}
+
+function scheduleDropTargets () {
+  let acc = []
+  for (let day = 0; day < DAYS; day++) {
+    for (let slot = SLOT_OFFSET; slot < SLOTS + SLOT_OFFSET; slot++) {
+      acc.push(
+        <ScheduleItem
+          day={day}
+          slot={slot}
+          duration={1}
+          key={`(${day}, ${slot})`}
+        >
+          <ScheduleDropTarget day={day} slot={slot} />
+        </ScheduleItem>
+      )
+    }
+  }
+  return acc
+}
+
 const gridScheduleStyle = {
   background: `repeating-linear-gradient(
     0deg,
@@ -21,47 +62,6 @@ const gridScheduleStyle = {
     hsla(0, 0%, 40%, 0.2) ${DAY_PERCENT}%,
     hsla(0, 0%, 40%, 0.2) ${DAY_PERCENT * 2}%
   )`
-}
-
-class BaseSchedule extends React.Component {
-  render () {
-    const {schedule} = this.props
-
-    return (
-      <div className='schedule' style={gridScheduleStyle} ref={(rootEl) => {
-        this.state = rootEl
-      }}>
-        {(() => {
-          let acc = []
-          for (let day = 0; day < DAYS; day++) {
-            for (let slot = SLOT_OFFSET; slot < SLOTS + SLOT_OFFSET; slot++) {
-              acc.push(
-                <ScheduleItem
-                  day={day}
-                  slot={slot}
-                  duration={1}
-                  key={`(${day}, ${slot})`}
-                >
-                  <ScheduleDropTarget day={day} slot={slot} />
-                </ScheduleItem>
-              )
-            }
-          }
-          return acc
-        })()}
-        {schedule.valueSeq().map((s) => (
-          <ScheduleItem
-            day={s.getIn(['time', 'day'])}
-            slot={s.getIn(['time', 'slot'])}
-            duration={s.get('duration')}
-            key={s.get('name')}
-          >
-            <Course name={s.get('name')} room={s.get('room')} />
-          </ScheduleItem>
-        )).toJS()}
-      </div>
-    )
-  }
 }
 
 const ScheduleConnector = connect(state => {
